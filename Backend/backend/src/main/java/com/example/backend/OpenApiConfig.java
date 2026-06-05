9de5fdc1180bc917f236ff070a842d673a8b4796
Dmitrii Chernikov <dima.chernikov.053@mail.ru>
2026-06-05 15:06:11 +0300

    Add authorize in Swagger

--- Backend/backend/src/main/java/com/example/backend/OpenApiConfig.java
+++ Backend/backend/src/main/java/com/example/backend/OpenApiConfig.java
@@ -1,7 +1,10 @@
 package com.example.backend;
 
-import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -12,8 +15,15 @@ public class OpenApiConfig {
     public OpenAPI customOpenAPI() {
         return new OpenAPI()
                 .info(new Info()
-                        .title("My API")
+                        .title("LDOE-Helper API")
                         .version("1.0")
-                        .description("Документация моего API"));
+                        .description("REST API для мобильного приложения LDOE-Helper"))
+                .addSecurityItem(new SecurityRequirement().addList("Bearer"))
+                .components(new Components()
+                        .addSecuritySchemes("Bearer", new SecurityScheme()
+                                .name("Bearer")
+                                .type(SecurityScheme.Type.HTTP)
+                                .scheme("bearer")
+                                .bearerFormat("JWT")));
     }
 }
